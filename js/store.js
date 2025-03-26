de3daa7300e6725a7f99f3ad68da454755e4c941
Velox <161338819+VeloxNF@users.noreply.github.com>
2025-03-26 21:35:22 +0700

    Add files via upload

--- js/store.js
+++ js/store.js
@@ -277,11 +277,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (orderData.paymentMethod === "QRIS") {
             document.getElementById("transaction-payment-qris").classList.remove("hidden");
+            document.getElementById("transaction-qris-image").src = `img/${orderData.paymentMethod.toLowerCase()}.png`;
             document.getElementById("transaction-payment-other").classList.add("hidden");
         } else {
             document.getElementById("transaction-payment-qris").classList.add("hidden");
             document.getElementById("transaction-payment-other").classList.remove("hidden");
-            document.getElementById("transaction-qris-image").src = `./img/qris.png`;
             document.getElementById("transaction-payment-logo").src = `img/${orderData.paymentMethod.toLowerCase()}.png`;
             document.getElementById("transaction-payment-number").textContent = "082351108031";
         }
